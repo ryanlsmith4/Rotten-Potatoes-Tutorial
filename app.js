fc62684df387135995af1a0fe2b071c228e24c34
Ryan Smith <ryan.l.smith4@gmail.com>
2018-08-30 13:57:40 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// set variables /configure express and handlebars
 const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
 
-
+//show
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.set('view engine', 'handlebars');
@@ -11,10 +12,21 @@ app.set('view engine', 'handlebars');
 //   res.send('Hello World!')
 // })
 
-app.get('/', (req, res) => {
-    res.render('home', {msg: 'hello world!'});
-})
+// app.get('/', (req, res) => {
+//     res.render('home', {msg: 'hello world!'});
+// })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
+})
+//Mock Array of Projects
+let reviews = [
+    { title: 'Great Review' },
+    { title: "Next Review" },
+    { title: "Check Out This one!" }
+]
+
+// Index
+app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
 })
